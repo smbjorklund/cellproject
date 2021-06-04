271006a49444ba45aa567d503a40ee7d89b9e167
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-06-04 13:42:27 +0200

    Remove video and audio -js libraries and modules

--- cellproject.net/modules/contrib/audiofield/js/wordpress.builder.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  AudioPlayer.setup('/sites/all/libraries/player/audio-player/player.swf', {
-    width: 400,
-    transparentpagebg: 'yes'
-  });
-  jQuery('.audiofield-wordpress-player') .each(function () {
-    AudioPlayer.embed(jQuery(this).attr('id'), {soundFile: jQuery(this).attr('data-src')});
-  });
-})(jQuery);
